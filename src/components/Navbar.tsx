834244f888aac5541aee41edc2f4b35f26182261
Mugtaba <mojtaba.mohamed187@gmail.com>
2025-01-14 18:26:50 +0200

    feat: add Navbar component with responsive mobile menu
    - Created Navbar.tsx component with hamburger menu for mobile responsiveness.
    - Added navbar.css for styling the Navbar component.
    - Included logo, hamburger, and cross icons in the Navbar.
    - Implemented toggle functionality for the mobile menu.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import '@styles/navbar.css'
+import logo from '@assets/logo.svg'
+import humburger from '@assets/humburger.svg'
+import cross from '@assets/cross.svg'
+import { useState } from 'react'
+
+const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {setIsOpen(!isOpen)};
+
+    return (
+        <>
+            <nav className="navbar container">
+                <div className="navbar__logo">
+                    <img className='navbar__logo-img' src={logo} alt="logo" />
+                    <span>KeepEyesOnSudan</span>
+                </div>
+
+                <div className='navbar__toggle' onClick={toggleMenu}>
+                    {isOpen ? <img className='navbar__toggle-img' src={cross} alt="cross" /> : <img className='navbar__toggle-img' src={humburger} alt="humburger" />}
+                </div>
+
+                <ul className={`navbar__links ${isOpen ? 'active' : ''}`}>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/statistics">Statistics</a></li>
+                    <li><a href="/news">News</a></li>
+                    <li><a href="/social">Social</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
