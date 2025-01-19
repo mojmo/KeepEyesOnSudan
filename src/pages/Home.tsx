2194bc9dedc4710424dbb62f77ca9a4f7b492b79
Mugtaba <mojtaba.mohamed187@gmail.com>
2025-01-20 01:05:59 +0200

    Add Summary component to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Hero from '@components/Hero';
 import Navbar from '@components/Navbar';
+import Summary from '@components/Summary';
 const Home = () => {
     return (
         <>
             <Navbar />
             <Hero />
+            <Summary />
         </>
     )
 }
