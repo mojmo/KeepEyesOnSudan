274f8d9c69ab77c8d9f643a13516bdb7848bb091
Mugtaba <mojtaba.mohamed187@gmail.com>
2025-01-25 00:31:18 +0200

    feat: style latest news section

--- src/components/LatestNews.tsx
+++ src/components/LatestNews.tsx
@@ -49,25 +49,31 @@ const LatestNews = () => {
 
     return (
         <section className="latest-news">
-            <div className="latest-news__header">
-                <h2>Latest News</h2>
-                <a href="/news" className="see-all-button">See All</a>
+            <div className="blurred__shape blurred__shape-left"></div>
+            <div className="blurred__shape blurred__shape-right"></div>
+            <div className="latest-news__content container">
+                <div className="latest-news__header">
+                    <h2>Latest News</h2>
+                    <a href="/news" className="see-all-button">See All</a>
+                </div>
+                <div className="latest-news__container">
+                    {news.map((article: any, index: number) => (
+                        <div key={index} className="latest-news__card">
+                            <img
+                                src={article.urlToImage || article_image}
+                                alt={article.title}
+                                className="latest-news__image"
+                            />
+                            <h3 className="latest-news__title">{article.title}</h3>
+                            <p className="latest-news__description">{article.description}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="latest-news__arrows">
+                    <button className="latest-news__arrow left" onClick={scrollLeft}>&lt;</button>
+                    <button className="latest-news__arrow right" onClick={scrollRight}>&gt;</button>
+                </div>
             </div>
-            <div className="latest-news__container">
-                {news.map((article: any, index: number) => (
-                    <div key={index} className="latest-news__card">
-                        <img
-                            src={article.urlToImage || article_image}
-                            alt={article.title}
-                            className="latest-news__image"
-                        />
-                        <h3 className="latest-news__title">{article.title}</h3>
-                        <p className="latest-news__description">{article.description}</p>
-                    </div>
-                ))}
-            </div>
-            <button className="latest-news__arrow left" onClick={scrollLeft}>&lt;</button>
-            <button className="latest-news__arrow right" onClick={scrollRight}>&gt;</button>
         </section>
     );
 }
