5c226af2fb461fbde32515045c5f7817720d832b
Mugtaba <mojtaba.mohamed187@gmail.com>
2025-01-24 23:21:30 +0200

    add latest news section to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Hero from '@components/Hero';
 import Navbar from '@components/Navbar';
 import Summary from '@components/Summary';
+import LatestNews from '@components/LatestNews';
 const Home = () => {
     return (
         <>
             <Navbar />
             <Hero />
             <Summary />
+            <LatestNews />
         </>
     )
 }
