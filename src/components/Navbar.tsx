56a50d1c5b6c0d15ef1caa03a16696d4298c9838
Mugtaba <mojtaba.mohamed187@gmail.com>
2025-01-27 00:33:18 +0200

    fix: make navbar sticky and ensure mobile menu stays visible
    - Added sticky positioning to the navbar to keep it visible when scrolling.
    - Fixed mobile menu positioning to ensure it stays visible and functional when scrolling.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,22 +12,24 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar container">
-                <div className="navbar__logo">
-                    <img className='navbar__logo-img' src={logo} alt="logo" />
-                    <span>KeepEyesOnSudan</span>
-                </div>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <div className="navbar__logo">
+                        <img className='navbar__logo-img' src={logo} alt="logo" />
+                        <span>KeepEyesOnSudan</span>
+                    </div>
 
-                <div className='navbar__toggle' onClick={toggleMenu}>
-                    {isOpen ? <img className='navbar__toggle-img' src={cross} alt="cross" /> : <img className='navbar__toggle-img' src={humburger} alt="humburger" />}
-                </div>
+                    <div className='navbar__toggle' onClick={toggleMenu}>
+                        {isOpen ? <img className='navbar__toggle-img' src={cross} alt="cross" /> : <img className='navbar__toggle-img' src={humburger} alt="humburger" />}
+                    </div>
 
-                <ul className={`navbar__links ${isOpen ? 'active' : ''}`}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/statistics">Statistics</a></li>
-                    <li><a href="/news">News</a></li>
-                    <li><a href="/social">Social</a></li>
-                </ul>
+                    <ul className={`navbar__links ${isOpen ? 'active' : ''}`}>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/statistics">Statistics</a></li>
+                        <li><a href="/news">News</a></li>
+                        <li><a href="/social">Social</a></li>
+                    </ul>
+                </div>
             </nav>
         </>
     )
