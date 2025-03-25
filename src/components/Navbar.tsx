323cd99b4b1379cb355d0eb717b79a1b51c0a679
Mugtaba <mojtaba.mohamed187@gmail.com>
2025-03-25 15:46:40 +0200

    fix: resolve responsiveness issues - hide overflowed blured shape. - Remove blue tap highlight on mobile using `tap-highlight-color: transparent` - Add CSS transitions for menu open/close animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,27 +11,27 @@ const Navbar = () => {
     const toggleMenu = () => {setIsOpen(!isOpen)};
 
     return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <div className="navbar__logo">
-                        <img className='navbar__logo-img' src={logo} alt="logo" />
-                        <span>KeepEyesOnSudan</span>
-                    </div>
-
-                    <div className='navbar__toggle' onClick={toggleMenu}>
-                        {isOpen ? <img className='navbar__toggle-img' src={cross} alt="cross" /> : <img className='navbar__toggle-img' src={humburger} alt="humburger" />}
-                    </div>
-
-                    <ul className={`navbar__links ${isOpen ? 'active' : ''}`}>
-                        <li><a href="#summary" onClick={() => setIsOpen(false)}>What's happening in Sudan?</a></li>
-                        <li><a href="#statistics" onClick={() => setIsOpen(false)}>Statistics</a></li>
-                        <li><a href="#latest-news" onClick={() => setIsOpen(false)}>Latest News</a></li>
-                        <li><a href="#social-media" onClick={() => setIsOpen(false)}>Social Media</a></li>
-                    </ul>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar__logo">
+                    <img className='navbar__logo-img' src={logo} alt="logo" />
+                    <span>KeepEyesOnSudan</span>
                 </div>
-            </nav>
-        </>
+
+                <div className='navbar__toggle' onClick={toggleMenu}>
+                    {isOpen ?
+                    <img className='navbar__toggle-img' src={cross} alt="close menu" /> :
+                    <img className='navbar__toggle-img' src={humburger} alt="open menu" />}
+                </div>
+
+                <ul className={`navbar__links ${isOpen ? 'active' : ''}`}>
+                    <li><a href="#summary" onClick={() => setIsOpen(false)}>What's happening in Sudan?</a></li>
+                    <li><a href="#statistics" onClick={() => setIsOpen(false)}>Statistics</a></li>
+                    <li><a href="#latest-news" onClick={() => setIsOpen(false)}>Latest News</a></li>
+                    <li><a href="#social-media" onClick={() => setIsOpen(false)}>Social Media</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
