c5d8ecda6fef0a3df08f1a8448ab76f0662bf24c
Mugtaba Mohamed <mojtaba.mohamed187@gmail.com>
2025-11-14 20:44:25 +0200

    feat: enhance footer layout and add donation links for better user engagement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,26 +13,48 @@ const Footer = () => {
             <div className="footer__container container">
                 <div className="footer__logo">
                     <h2>{t('footer.title')}</h2>
+                    <p>{t('footer.description')}</p>
                 </div>
                 <div className="footer__content">
-                    <div className="footer__links">
-                        <p>{t('footer.description')}</p>
-                        <a className="tooltip" href="https://x.com/sudan_war" target="_blank">
-                            {t('footer.sudanWarUpdates')}
-                            <span className="tooltiptext">{t('footer.sudanWarUpdatesDescription')}</span>
-                        </a>
-                        <a className="tooltip" href="https://x.com/AlMigdadHassan0" target="_blank">
-                            {t('footer.almigdadHassan')}
-                            <span className="tooltiptext">{t('footer.almigdadHassanDescription')}</span>
-                        </a>
-                        <a className="tooltip" href="https://x.com/BSonblast" target="_blank">
-                            {t('footer.saraElhassan')}
-                            <span className="tooltiptext">{t('footer.saraElhassanDescription')}</span>
-                        </a>
-                        <a className="tooltip" href="https://x.com/KhartoumKitchen" target="_blank">
-                            {t('footer.khartoumAidKitchen')}
-                            <span className="tooltiptext">{t('footer.khartoumAidKitchenDescription')}</span>
-                        </a>
+                    <div className="footer__links-container">
+                        <div className="footer__links">
+                            <h3>{t('footer.updates')}</h3>
+                            <a className="tooltip" href="https://x.com/sudan_war" target="_blank">
+                                {t('footer.sudanWarUpdates')}
+                                <span className="tooltiptext">{t('footer.sudanWarUpdatesDescription')}</span>
+                            </a>
+                            <a className="tooltip" href="https://x.com/AlMigdadHassan0" target="_blank">
+                                {t('footer.almigdadHassan')}
+                                <span className="tooltiptext">{t('footer.almigdadHassanDescription')}</span>
+                            </a>
+                            <a className="tooltip" href="https://x.com/BSonblast" target="_blank">
+                                {t('footer.saraElhassan')}
+                                <span className="tooltiptext">{t('footer.saraElhassanDescription')}</span>
+                            </a>
+                            <a className="tooltip" href="https://x.com/VistaMaps" target="_blank">
+                                {t('footer.vistaMaps')}
+                                <span className="tooltiptext">{t('footer.vistaMapsDescription')}</span>
+                            </a>
+                        </div>
+                        <div className="footer__links">
+                            <h3>{t('footer.donnations')}</h3>
+                            <a className="tooltip" href="https://sapa-usa.org/Hope-for-Sudan/" target="_blank">
+                                {t('footer.SAPA')}
+                                <span className="tooltiptext">{t('footer.SAPADescription')}</span>
+                            </a>
+                            <a className="tooltip" href="https://sadagaat-usa.org/" target="_blank">
+                                {t('footer.sadagaat')}
+                                <span className="tooltiptext">{t('footer.sadagaatDescription')}</span>
+                            </a>
+                            <a className="tooltip" href="https://www.khartoumaidkitchen.org/" target="_blank">
+                                {t('footer.khartoumAidKitchen')}
+                                <span className="tooltiptext">{t('footer.khartoumAidKitchenDescription')}</span>
+                            </a>
+                            <a className="tooltip" href="https://linktr.ee/basmat_wasl" target="_blank">
+                                {t('footer.basmatWasl')}
+                                <span className="tooltiptext">{t('footer.basmatWaslDescription')}</span>
+                            </a>
+                        </div>
                     </div>
                     <div className="footer__copyright">
                         <p>{t('footer.copyright')} <a href="https://github.com/mojmo" target="_blank"><strong>{t('footer.mugtaba')}</strong></a></p>
